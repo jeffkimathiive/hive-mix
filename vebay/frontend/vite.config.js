228dc68fb051b858cb459c7df8b4859fe5c1d0f6
Jaemin Kim <jeff.kim@hiive.com>
2026-03-20 14:14:41 -0700

    update vite.config.js

--- vebay/frontend/vite.config.js
+++ vebay/frontend/vite.config.js
@@ -4,4 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    headers: {
+      'X-Frame-Options': 'ALLOWALL',
+    },
+    proxy: {
+      '/tidewave': 'http://localhost:4000',
+    },
+  },
 })
